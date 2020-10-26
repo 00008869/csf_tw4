051f0e419f2c781e0cd7ce081080ba4282bc52ac
00008869 <00008869wiut@gmail.com>
2020-10-26 10:50:24 +0500

    two functions with countdown/up

--- count.py
+++ count.py
@@ -1,1 +1,15 @@
 n = int(input("write your number: "))
+
+def countdown(n):
+    if n <= 0:
+        print('Blastoff!')
+    else:
+        print(n)
+        countdown(n-1)
+
+def countup(n):
+    if n >= 0:
+        print('Blastoff!')
+    else:
+        print(n)
+        countup(n+1)
